990202a4eb6d5579aecd65d41aa3780ea6280533
Nuri Lacka <nurilacka@gmail.com>
2025-07-25 11:55:35 +0200

    Update sidebar icons: replace DataSources with ElectricalServices icon and enhance DataSources component with theme support

--- src/renderer/components/sidebar/icons.tsx
+++ src/renderer/components/sidebar/icons.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { CodeSharp, Cable, Assignment, Cloud } from '@mui/icons-material';
+import {
+  CodeSharp,
+  Cable,
+  Assignment,
+  Cloud,
+  ElectricalServices as ElectricalServicesIcon,
+} from '@mui/icons-material';
+import { useTheme } from '@mui/material/styles';
 import { icons } from '../../../../assets';
 
 const style = {
@@ -10,23 +17,26 @@ const style = {
   height: '24px',
 };
 
-export const DataSources: React.FC = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    width={24}
-    height={24}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
-    />
-  </svg>
-);
+export const DataSources: React.FC = () => {
+  const theme = useTheme();
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke={theme.palette.primary.main}
+      width={24}
+      height={24}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
+      />
+    </svg>
+  );
+};
 
 export const Connections: React.FC = () => <Cable color="primary" />;
 
@@ -50,4 +60,8 @@ export const DBTProjects: React.FC = () => (
     alt="dbt projects"
     style={{ ...style, width: 24, height: 24, marginLeft: 2 }}
   />
+);
+
+export const ElectricalServices: React.FC = () => (
+  <ElectricalServicesIcon color="primary" />
 );
