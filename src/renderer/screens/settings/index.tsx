aae5c646c332d6f331263d5eb819a83fb5c398aa
flakronademi <flakronflaki0@gmail.com>
2026-07-31 12:53:48 +0200

    fix: added scroll in settings sidebar

--- src/renderer/screens/settings/index.tsx
+++ src/renderer/screens/settings/index.tsx
@@ -170,7 +170,7 @@ const Settings: React.FC = () => {
             height: '100%',
           }}
         >
-          <Box>
+          <Box sx={{ flex: 1, minHeight: 0, overflowX: 'hidden', overflowY: 'auto' }}>
             <Box
               sx={{
                 display: 'flex',
